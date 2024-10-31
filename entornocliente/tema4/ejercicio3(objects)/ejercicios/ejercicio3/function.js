f07da60570b901020bc2f01ebaaaf5917e6314cc
luisdavid26 <luisdavidm2626@gmail.com>
2024-10-31 11:32:02 +0100

    predata-artist

--- entornocliente/tema4/ejercicio3(objects)/ejercicios/ejercicio3/function.js
+++ entornocliente/tema4/ejercicio3(objects)/ejercicios/ejercicio3/function.js
@@ -1,28 +1,31 @@
-function artistasSolistas(artistas) {
+export function artistasSolistas(artistas) {
   return artistas.filter((artista) => artista.solista);
 }
-function artistasPorEdad(edad, artistas) {
+export function artistasPorEdad(edad, artistas) {
   return artistas.filter((artista) => artista.edad === edad);
 }
-function cantidadDeArtistasPorInstrumento(artistas) {
+export function cantidadDeArtistasPorInstrumento(artistas) {
   return artistas.reduce((cont, artista) => {
     const instrumento = artista.instrumento;
     cont[instrumento] = cont[instrumento] ? cont[instrumento] + 1 : 1;
     return cont;
   }, {});
 }
-function cantidadDeArtistasPorGenero(artistas) {
+export function cantidadDeArtistasPorGenero(artistas) {
   return artistas.reduce((cont, artista) => {
     const genero = artista.genero;
     cont[genero] = cont[genero] ? cont[genero] + 1 : 1;
     return cont;
   }, {});
 }
-function artistasConMasDiscosQue(cantidadDeDiscos,artistas) {
+export function artistasConMasDiscosQue(cantidadDeDiscos,artistas) {
     return artistas.filter(artista=>artista.discos&&artista.discos.length > cantidadDeDiscos)
     .sort((a,b)=>b.discos.length-a.discos.length);
 }
-function artistaConMasEntradasVendidas(artistas) {
+export function artistaConMasEntradasVendidas(artistas) {
     return artistas.filter(artista=>artista.entradasVendidas&&artista.entradasVendidas.length > entradasVendidas)
     .sort((a,b)=>b.entradasVendidas.length-a.entradasVendidas.length);
+}
+export function artistaConMayorRecaudacion(params) {
+  
 }
