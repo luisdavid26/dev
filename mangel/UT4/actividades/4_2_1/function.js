37ae662e57680628c27a6811bec3c30569c4bc6e
mangel <mangelvelo@gmail.com>
2024-11-05 09:07:23 +0100

    commit with new functions

--- mangel/UT4/actividades/4_2_1/function.js
+++ mangel/UT4/actividades/4_2_1/function.js
@@ -1,27 +1,55 @@
+/**
+ * 
+ * @param {*} artistas 
+ * @returns 
+ */
 function artistasSolistas(artistas) {
     return artistas.filter((artista) => artista.solista);
 }
-
+/**
+ * 
+ * @param {*} edad 
+ * @param {*} artistas 
+ * @returns 
+ */
 function artistasPorEdad(edad, artistas) {
     return artistas.filter((artista) => artista.edad == edad);
 }
-
+/**
+ * 
+ * @param {*} artistas 
+ * @returns 
+ */
 function cantidadDeArtistasPorInstrumento(artistas) {
     return artistas.reduce(((contador, artista) => {
-        
+        let instrumento = artista.instrumento;
+        contador[instrumento] = contador[instrumento] ? contador[instrumento] + 1 : 1;
     }));
 }
-
-function cantidadDeArtistasPorGenero() {
-    
+/**
+ * 
+ * @param {*} artista 
+ * @returns 
+ */
+function cantidadDeArtistasPorGenero(artistas) {
+    return artistas.reduce(((contador, artista) => {
+        let genero = artista.genero;
+        contador[genero] = contador[genero] ? contador[genero] + 1 : 1;
+    }));
+}
+/**
+ * 
+ * @param {*} artistas 
+ * @param {*} cantidadDiscos 
+ * @returns 
+ */
+function artistasConMasDiscosQue(artistas, cantidadDiscos) {
+    return artistas.filter(artista => artista.discos && artista.discos.length > cantidadDiscos)
+        .sort((a,b) => b.discos.length - a.discos.length);
 }
 
-function artistasConMasDiscosQue() {
-    
-}
-
-function artistaConMasEntradasVendidas() {
-    
+function artistaConMasEntradasVendidas(artistas) {
+    return 
 }
 
 function artistaConMayorRecaudacion() {
