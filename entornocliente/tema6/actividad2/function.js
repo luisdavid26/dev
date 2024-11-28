293325f6d78bf32838a1c5c6ffe67dd93870ead4
luisdavid26 <luisdavidm2626@gmail.com>
2024-11-28 12:13:05 +0100

    .

--- entornocliente/tema6/actividad2/function.js
+++ entornocliente/tema6/actividad2/function.js
@@ -4,8 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const apellidos = document.getElementById("apellidos");
   const edad = document.getElementById("edad");
   const nif = document.getElementById("NIF");
+  const selector = document.getElementsByName("provincias");
+
   const texterrores = document.getElementById("errores");
-  const RegExp = /[0-9]{8}-[a-z]/;
+  //la expresion regular del nif es para que los primero 8 caracteres tienen que ser numeros
+  //despues de los 8 primero digitos viene seguido de un guion y una letra
+  const RegExpNIF = /^[0-9]{8}-[A-Za-z]$/;
+  //la expresion regular para el email , tiene que en la primera parte puedes poner letras o numeros
+  //seguido en un @ puedes poner letras o numero y al poner un punto puedes poner solo p
+  const RegExpemail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
+
   form.addEventListener("submit", (event) => {
     let errores = [];
     texterrores.textContent = ""; // Limpiar mensajes previos
@@ -41,10 +49,26 @@ document.addEventListener("DOMContentLoaded", () => {
       errores.push("El NIF es obligatorio");
       nif.focus();
     }
-    if (!RegExp.test(nif)) {
+    if (!RegExpNIF.test(nif.value) && errores.length === 0) {
       errores.push("El NIF no es valido");
       nif.focus();
     }
+    //actividad 4
+    if (email.value === "" && errores.length === 0) {
+      errores.push("El email es obligatorio");
+      email.focus();
+    }
+    if (!RegExpemail.test(email.value) && errores.length === 0) {
+      errores.push("El email no es válido");
+      email.focus();
+    }
+    //actividad5
+    selector.addEventListener("change", (e) => {
+      if (this.value === "--" && errores.length === 0) {
+        errores.push("Tienes que elegir una provincia obligatoriamente");
+        selector.focus();
+      }
+    });
 
     if (errores.length > 0) {
       event.preventDefault();
