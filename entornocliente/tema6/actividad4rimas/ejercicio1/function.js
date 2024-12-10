7725e468160f96e2966c1eadc450fedd2d665ca6
luisdavid26 <luisdavidm2626@gmail.com>
2024-12-10 01:30:10 +0100

    todo ultimaparte

--- entornocliente/tema6/actividad4rimas/ejercicio1/function.js
+++ entornocliente/tema6/actividad4rimas/ejercicio1/function.js
@@ -1,45 +1,72 @@
 let diccionario = new Map();
 
-function agregarRima(palabra, rima) {
+function agregarrima(palabra, rima) {
   if (!palabra || !rima) {
     console.log("Ambos campos, palabra y rima, son obligatorios.");
     return;
   }
+
   if (diccionario.has(palabra)) {
-    diccionario.get(palabra).push(rima); // Agregamos la nueva rima
+    const rimas = diccionario.get(palabra);
+    if (rimas.includes(rima)) {
+      console.log(`La rima ${rima} ya existe para la palabra ${palabra}.`);
+    } else {
+      rimas.push(rima); 
+      console.log(`La rima ${rima} ha sido agregada a la palabra ${palabra}.`);
+    }
   } else {
-    diccionario.set(palabra, [rima]); // Creamos una nueva entrada con la palabra y la rima
+    diccionario.set(palabra, [rima]);
+    console.log(`La palabra ${palabra} ha sido agregada con su rima ${rima}.`);
   }
-  console.log(`La rima "${rima}" ha sido agregada a la palabra "${palabra}".`);
 }
 
-function obtenerRimas(palabra) {
+
+function obtenerrimas(palabra) {
   if (!palabra) {
     console.log("El campo palabra es obligatorio.");
-    return;
+    return false;
   }
   if (diccionario.has(palabra)) {
-    return diccionario.get(palabra); // Devolvemos las rimas asociadas a la palabra
+    return diccionario.get(palabra); 
   } else {
-    console.log(`La palabra "${palabra}" no existe en el diccionario.`);
+    console.log(`La palabra ${palabra} no existe en el diccionario.`);
+  return false;
   }
 }
 
-function eliminarRima(palabra, rima) {
+function eliminarrima(palabra, rima) {
   if (!palabra || !rima) {
-    console.log("Ambos campos, palabra y rima, son obligatorios.");
+    console.log("Ambos campos,son obligatorios.");
     return;
   }
   if (diccionario.has(palabra)) {
     const rimas = diccionario.get(palabra).filter((rimaborrar) => rimaborrar !== rima);
     if (rimas.length > 0) {
-      diccionario.set(palabra, rimas); // Actualizamos las rimas
-      console.log(`La rima "${rima}" ha sido eliminada de la palabra "${palabra}".`);
-    } else {
-      diccionario.delete(palabra); // Eliminamos la palabra si ya no tiene rimas
-      console.log(`La palabra "${palabra}" ha sido eliminada, ya que no tiene más rimas.`);
+      diccionario.set(palabra, rimas);
+      console.log(`La rima ${rima} ha sido eliminada de la palabra ${palabra}.`);
+      return true;
+    } 
+  } else {
+    console.log(`La palabra ${palabra} no existe en el diccionario.`);
+    return false;
+  }
+}
+
+function eliminarpalabra(palabra) {
+  if (!palabra) {
+    console.log(" la palabra es obligatoria.");
+    return;
+  }
+  if (diccionario.has(palabra)) {
+    const palabravar = diccionario.get(palabra);
+    if (palabravar.length > 0) {
+      diccionario.delete(palabra); // eliminamos la palabra 
+      console.log(`La palabra ${palabra} ha sido eliminada`);
+      return true;
     }
   } else {
-    console.log(`La palabra "${palabra}" no existe en el diccionario.`);
+    console.log(`La palabra ${palabra} no existe en el diccionario.`);
+    return false;
+
   }
 }
--- entornocliente/tema6/actividad4rimas/ejercicio1/index.js
+++ entornocliente/tema6/actividad4rimas/ejercicio1/index.js
@@ -1,26 +1,132 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const formulario = document.getElementById("newEvent");
-  const campoaddpalabra= document.getElementById("addpalabra");
+  const formularioaddrima = document.getElementById("addrimaform");
+  const formulariogetrima = document.getElementById("getrimaform");
+  const formulariormvrima = document.getElementById("rmvrimaform");
+  const formulariormvplabra = document.getElementById("rmvpalabraform");
+  const campoaddpalabra = document.getElementById("addpalabra");
+  const campoaddrima = document.getElementById("addrima");
+  const campogetrima = document.getElementById("getrimas");
+  const campopalabra = document.getElementById("palabra");
+  const campormvrima = document.getElementById("rmvrima");
+  const listagetrima = document.getElementById("listarimas");
+  const campormvpalabra = document.getElementById("rmvpalabra");
 
-  formulario.addEventListener("submit", (e) => {
+  const nombreReg = /^[A-Za-z]{1,}$/; // Expresión regular para validar palabras (solo letras)
+
+  formularioaddrima.addEventListener("submit", (e) => {
     e.preventDefault();
-    let esvalido = true;
+    let palabravalida = true;
+    let rimavalida = true;
 
-    //ejercicio a y b
-    //comprobamos si esta vacio
-    if (!campoaddpalabra.value||!nombreReg.test(campoaddpalabra.value)) {
+    if (!campoaddpalabra.value || !nombreReg.test(campoaddpalabra.value)) {
       campoaddpalabra.classList.add("is-invalid");
       campoaddpalabra.classList.remove("is-valid");
-      esvalido = false;
+      palabravalida = false;
     } else {
       campoaddpalabra.classList.add("is-valid");
       campoaddpalabra.classList.remove("is-invalid");
+      palabravalida = true;
     }
 
+    if (palabravalida) {
+      if (!campoaddrima.value || !nombreReg.test(campoaddrima.value)) {
+        campoaddrima.classList.add("is-invalid");
+        campoaddrima.classList.remove("is-valid");
+        rimavalida = false;
+      } else {
+        agregarrima(campoaddpalabra.value, campoaddrima.value);
+        campoaddrima.classList.add("is-valid");
+        campoaddrima.classList.remove("is-invalid");
+        rimavalida = true;
+      }
+    }
+  });
+  formulariogetrima.addEventListener("submit", (e) => {
+    e.preventDefault();
+    palabravalida = false;
+    let rimasvar;
+    if (!campogetrima.value || !nombreReg.test(campogetrima.value)) {
+      campogetrima.classList.add("is-invalid");
+      campogetrima.classList.remove("is-valid");
+      palabravalida = false;
+    } else {
+      while (listagetrima.firstChild) {
+        //limpiamos la lista
+        listagetrima.removeChild(listagetrima.firstChild);
+      }
+      rimasvar = obtenerrimas(campogetrima.value);
 
-    
+      if (rimasvar.length > 0) {
+        rimasvar.forEach((rima) => {
+          const li = document.createElement("li");
+          li.textContent = rima;
+          listagetrima.appendChild(li);
+        });
+      } else {
+        const li = document.createElement("li");
+        li.textContent = "No hay rimas para esta palabra.";
+        listagetrima.appendChild(li);
+      }
 
+      campogetrima.classList.add("is-valid");
+      campogetrima.classList.remove("is-invalid");
+      palabravalida = true;
+    }
+  });
+
+  formulariormvrima.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let palabravalida = true;
+    let rimavalida = true;
+
+    if (!campopalabra.value || !nombreReg.test(campopalabra.value)) {
+      campopalabra.classList.add("is-invalid");
+      campopalabra.classList.remove("is-valid");
+      palabravalida = false;
+    } else {
+      campopalabra.classList.add("is-valid");
+      campopalabra.classList.remove("is-invalid");
+      palabravalida = true;
+    }
+
+    if (palabravalida) {
+      if (!campormvrima.value || !nombreReg.test(campormvrima.value)) {
+        campormvrima.classList.add("is-invalid");
+        campormvrima.classList.remove("is-valid");
+        rimavalida = false;
+      } else {
+        if (eliminarrima(campormvpalabra.value, campormvrima.value)) {
+          campormvrima.classList.add("is-valid");
+          campormvrima.classList.remove("is-invalid");
+          rimavalida = true;
+        } else {
+          campormvrima.classList.add("is-invalid");
+          campormvrima.classList.remove("is-valid");
+        }
+      }
+    }
+  });
+  formulariormvplabra.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let palabravalida = true;
+    if (!campormvpalabra.value || !nombreReg.test(campormvpalabra.value)) {
+      campormvpalabra.classList.add("is-invalid");
+      campormvpalabra.classList.remove("is-valid");
+      palabravalida = false;
+    } else {
+      if (eliminarpalabra(campormvpalabra.value)) {
+        campormvpalabra.classList.add("is-valid");
+        campormvpalabra.classList.remove("is-invalid");
+        palabravalida = true;
+      } else {
+        campormvpalabra.classList.add("is-invalid");
+        campormvpalabra.classList.remove("is-valid");
+        palabravalida = false;
+      }
+    }
   });
 
 
+
+  
 });
